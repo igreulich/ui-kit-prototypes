62b25e9d64bc4a1ccbb7828845f18664c02107fc
Jef Lippiatt <jeflippiatt@gmail.com>
2017-09-27 15:48:44 -0400

    added expand collapse
    added table that has expand and collapse, added expand / collapse icon styles, added js row expand fiel

---
+++ tables/table-expand.js
@@ -0,0 +1,10 @@
+// Table Expand Row Script
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+ acc[i].onclick = function(){
+ this.classList.toggle("active");
+ this.nextElementSibling.classList.toggle("panel");
+};
+}
