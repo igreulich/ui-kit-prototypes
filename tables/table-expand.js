a365b02f4eea39765981ae5e260e375d2c14a55c
Jef Lippiatt <jeflippiatt@gmail.com>
2017-09-28 15:35:35 -0400

    Improved table pagination and expand collapse

--- tables/table-expand.js
+++ tables/table-expand.js
@@ -1,8 +1,8 @@
 // Table Expand Row Script
 
 var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
+
+for (var i = 0; i < acc.length; i++) {
  acc[i].onclick = function(){
  this.classList.toggle("active");
  this.nextElementSibling.classList.toggle("panel");
